1409e9aec207dbfa6b7743148fb96717881d7fc0
WVLeeuw <w.v.leeuw@student.tue.nl>
2016-12-04 20:49:54 +0100

    Update elections.js

--- elections.js
+++ elections.js
@@ -1,6 +1,5 @@
 $("document").ready(function() {
-  $('.btn').on('click', function() {
-    var infoId = $(this).attr("data-infoId");
-    $('#'+infoId).slideToggle(200);
+  $("#accordion").accordion(  {
+    collapsible:true 
   });
 });
